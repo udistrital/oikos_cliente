a56bb1196e41ae5f9b154c85bc70d90c45f59e36
jsreyes <ingjsreyesm@gmail.com>
2017-04-19 19:03:58 -0500

    Ajuste controlador

--- app/scripts/controllers/espacio_fisico/crear_espacio_fisico.js
+++ app/scripts/controllers/espacio_fisico/crear_espacio_fisico.js
@@ -8,10 +8,31 @@
  * Controller of the oikosClienteApp
  */
 angular.module('oikosClienteApp')
-  .controller('CrearEspacioFisicoCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CrearEspacioFisicoCtrl', function (oikosRequest, $scope) {
+
+      /*Función para insertar espacios fisicos*/
+      $scope.confirmar = function() {
+        var json = {
+          "Nombre": $scope.espacio_fisico.nombre,
+          "Codigo": $scope.espacio_fisico.telefono,
+          "Correo": $scope.espacio_fisico.correo
+        };
+
+        //Registrar Espacio Físico
+        oikosRequest.post('espacio_fisico', json)
+          .then(function() {
+            alert("Guardo exitosamente");
+            //Limpia los campos despues de hacer una inserción
+            $scope.espacio_fisico = {};
+          });
+      }
+
+      /*Función para limpiar todos los campos del formulario con el botón "Cancelar"*/
+      $scope.reset = function(form) {
+        $scope.espacio_fisico = {};
+        if (form) {
+          form.$setPristine();
+          form.$setUntouched();
+        }
+      };
   });
