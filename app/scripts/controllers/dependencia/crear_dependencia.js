426f807181e7cee9072a256d6239cc5853755200
Javier Reyes <ingjsreyesm@gmail.com>
2017-05-03 17:19:57 -0500

    Se ajusta el controlador y la vista de crear dependencia

--- app/scripts/controllers/dependencia/crear_dependencia.js
+++ app/scripts/controllers/dependencia/crear_dependencia.js
@@ -8,31 +8,45 @@
  * Controller of the oikosClienteApp
  */
 angular.module('oikosClienteApp')
-  .controller('CrearDependenciaCtrl', function (oikosRequest, $scope) {
-    /*Función para insertar dependenciaes*/
-    $scope.confirmar = function() {
-      var json = {
-        "Nombre": $scope.dependencia.nombre,
-        "Telefono": $scope.dependencia.telefono,
-        "Correo": $scope.dependencia.correo
-      };
+  .controller('CrearDependenciaCtrl', function (oikosRequest) {
+    //Se utiliza la variable self estandarizada
+    var self=this;
+    //Se crea JSON para la nueva_dependencia
+    self.nueva_dependencia = {};
 
-      //Registrar Aplicación
-      oikosRequest.post('dependencia', json)
-        .then(function() {
-          alert("Guardo exitosamente");
-          //Limpia los campos despues de hacer una inserción
-          $scope.dependencia = {};
-        });
-    }
+    /*//Creación tabla que carga edificios
+     self.gridOptions_dependencias = {
+        enableRowSelection: true,
+        enableRowHeaderSelection: true,
+        enableSelectAll: true,
+        columnDefs: [
+          { field: 'Nombre'},
+          { field: 'Correo'}
+     ],
+   };
 
-    /*Función para limpiar todos los campos del formulario con el botón "Cancelar"*/
-    $scope.reset = function(form) {
-      $scope.dependencia = {};
-      if (form) {
+    //Función obtener los edificios
+    oikosRequest.get('dependencia', $.param({
+       limit: 0
+    }))
+    .then(function(response) {
+       self.gridOptions_edificios.data = response.data;
+    });*/
+
+    //Función para crear la dependencia
+    self.crear_dependencia=function(form){
+      console.log(self.nueva_dependencia);
+      //Petición POST
+      oikosRequest.post("dependencia", self.nueva_dependencia).then(function(response){
+        //Notificación de success
+        swal("", "Se agregó con éxito la sede <b>" + self.nueva_dependencia.Nombre + "</b> con télefono <b>" +
+        self.nueva_dependencia.Telefono + "</b>", "success");
+
+        //Reinicia las variables y restablece el formulario
+        self.nueva_dependencia={};
         form.$setPristine();
         form.$setUntouched();
-      }
-    };
+      });
+  };
 
   });
