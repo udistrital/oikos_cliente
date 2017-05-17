23b0b28977ddaca4bf7d29baaf951c439372cd2d
Javier Reyes <ingjsreyesm@gmail.com>
2017-05-16 20:24:07 -0500

    Se ajusta uso de mayusculas, alerta despues de registro que permite registrar una nueva dependencia o consultarla.

--- app/scripts/controllers/dependencia/crear_dependencia.js
+++ app/scripts/controllers/dependencia/crear_dependencia.js
@@ -36,29 +36,36 @@ angular.module('oikosClienteApp')
     //Función para crear la dependencia
     self.crear_dependencia=function(form){
       console.log(self.nueva_dependencia);
+
+      //Convertir a mayusculas
+      self.nueva_dependencia.Nombre = self.nueva_dependencia.Nombre.toUpperCase();
+      self.nueva_dependencia.Telefono = self.nueva_dependencia.Telefono.toUpperCase();
+      self.nueva_dependencia.Correo = self.nueva_dependencia.Correo.toUpperCase();
+
       //Petición POST
       oikosRequest.post("dependencia", self.nueva_dependencia).then(function(response){
         //Notificación de success
         swal({
+          title: "Registro exitoso",
           html: "<label>Se insertó correctamente la dependencia con los siguientes datos</label><br><br><label><b>Nombre:</b></label> "
           + self.nueva_dependencia.Nombre+"<br><label><b>Télefono:</b></label> " + self.nueva_dependencia.Telefono +
           "<br><label><b>Correo electrónico:</b></label>" + self.nueva_dependencia.Correo,
           type: "success",
           showCancelButton: true,
           confirmButtonColor: "#449D44",
-          cancelButtonColor: "#C9302C",
-          confirmButtonText: "Consultar dependencia",
-          cancelButtonText: "Registrar nueva dependencia",
+          cancelButtonColor: "#2c6bc9",
+          confirmButtonText: "Registrar nueva dependencia",
+          cancelButtonText: "Consultar dependencia",
         }).then(function() {
-              //Si da click lo redirije a consultar dependencias
+            //Si da click lo redirije a crear nueva dependencia
+            $window.location.reload();
+          },
+          function(dismiss) {
+            if (dismiss === 'cancel') {
+                //Si da click lo redirije a consultar dependencia
               $window.location.href = '#/consultar_dependencia';
-            }, function(dismiss) {
-              //Si da click lo redirije a crear_dependencia
-              if (dismiss === 'cancel') {
-                /*Función para limpiar todos los campos del formulario con el botón "Cancelar"*/
-                $window.location.href = '#/crear_dependencia';
-              }
-            })
+            }
+          })
       });
   };
   });
